fcced6a84a6b3d021daf4edaa4039ffbb942dfd4
Moon4824 <salomonbovel@gmail.com>
2025-07-14 15:08:26 +0300

    upload

--- projetfinal/pages/upload.php
+++ projetfinal/pages/upload.php
@@ -3,34 +3,36 @@ include('../inc/header.php');
 $id = $_SESSION['Connecter']['id_membre'];
 $liste_categorie = getListe_categorie();
 ?>
+<div class="form-container mx-auto " style="margin-top: 80px;">
+    <h2>Ajouter un nouvel objet</h2>
+    <form action="../traitement/traiteupload.php" method="POST" enctype="multipart/form-data">
 
-<h2>Ajouter un nouvel objet</h2>
-<form action="../traitement/traiteupload.php" method="POST" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="nom_objet">Nom de l'objet</label>
+            <input type="text" id="nom_objet" name="nom_objet" required>
 
-    <div class="form-group">
-        <label for="nom_objet">Nom de l'objet</label>
-        <input type="text" id="nom_objet" name="nom_objet" required>
+        </div>
 
-    </div>
-
-    <div class="form-group">
-        <label for="categorie">Catégorie</label>
-        <select id="categorie" name="categorie" required>
-            <?php foreach ($liste_categorie as $categorie) { ?>
+        <div class="form-group">
+            <label for="categorie">Catégorie</label>
+            <select id="categorie" name="categorie" required>
+                <?php foreach ($liste_categorie as $categorie) { ?>
                 <option value="<?= $categorie['id_categorie'] ?>"><?= $categorie['nom_categorie'] ?></option>
-            <?php } ?>
-        </select>
-    </div>
+                <?php } ?>
+            </select>
+        </div>
 
 
-    <div class="form-group">
-        <label for="media">Images de l'objet (plusieurs sélections possibles) </label>
-        <input type="file" id="images" name="images[]" accept="image/*" required multiple>
-        <small class="form-text text-muted">La première image téléchargée sera l'image principale.</small>
+        <div class="form-group">
+            <label for="media">Images de l'objet (plusieurs sélections possibles) </label>
+            <input type="file" id="images" name="images[]" accept="image/*" required multiple>
+            <small class="form-text text-muted">La première image téléchargée sera l'image principale.</small>
 
-    </div>
+        </div>
 
-    <input type="hidden" name="id_membre" value="<?= $id ?>">
+        <input type="hidden" name="id_membre" value="<?= $id ?>">
 
-    <button type="submit" class="btn-upload">Add</button>
-</form>
+        <button type="submit" class="btn-upload">Add</button>
+    </form>
+
+</div>
