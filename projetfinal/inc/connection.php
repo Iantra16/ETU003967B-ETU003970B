f91e381df9b1fe25c2969fbeb14eeb5a97332c33
Iantra16 <iantrarak@gmail.com>
2025-07-14 15:20:57 +0300

    con

--- projetfinal/inc/connection.php
+++ projetfinal/inc/connection.php
@@ -4,8 +4,8 @@ function dbconnect()
     static $bdd = null;
 
     if ($bdd === null) {
-        $bdd = mysqli_connect('localhost', 'root', '', 'db_s2_ETU003970');
-        // $bdd = mysqli_connect('localhost', 'ETU003970', '2qVrbh7m', 'db_s2_ETU003970');
+        //$bdd = mysqli_connect('localhost', 'root', '', 'db_s2_ETU003970');
+        $bdd = mysqli_connect('localhost', 'ETU003970', '2qVrbh7m', 'db_s2_ETU003970');
 
         if (!$bdd) {
             die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
